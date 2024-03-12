8964ac16b77d6eb307c96f7fb2847530101dc72a
Sam1ndaFernando <uthpala2001@gmail.com>
2024-03-12 20:53:03 +0530

    adminDao complete

--- src/main/java/lk/ijse/dao/custom/impl/AdminDaoImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/AdminDaoImpl.java
@@ -1,4 +1,69 @@
 package lk.ijse.dao.custom.impl;
 
-public class AdminDaoImpl {
+import lk.ijse.dao.BaseDao;
+import lk.ijse.dao.custom.AdminDao;
+import lk.ijse.entity.Admin;
+import org.hibernate.query.Query;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static lk.ijse.dao.BaseDao.executeTransaction;
+
+public class AdminDaoImpl implements AdminDao {
+    @Override
+    public void save(Admin entity) throws SQLException {
+        executeTransaction(session -> session.save(entity));
+    }
+
+    @Override
+    public void update(Admin entity) throws SQLException {
+        executeTransaction(session -> {
+            session.update(entity);
+            return null;
+        });
+    }
+
+    @Override
+    public void delete(int id) throws SQLException {
+        executeTransaction(session -> {
+            Admin entity = null;
+            try {
+                entity = getbyId(id);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            session.delete(entity);
+            return null;
+        });
+    }
+
+    @Override
+    public List<Admin> loadAll() throws SQLException {
+        return   executeTransaction(session -> {
+            Query query = session.createQuery("FROM Admin");
+            return query.getResultList();
+
+        });
+    }
+
+    @Override
+    public Admin get(String data) throws SQLException {
+        return executeTransaction(session -> {
+            Query query = session.createQuery("from Admin where userName=:username");
+            query.setParameter("username", data);
+            List<Admin> admins= query.getResultList();
+            for (Admin admin : admins){
+                return admin;
+            }
+            System.out.println("ok");
+            return null;
+        });
+    }
+
+    @Override
+    public Admin getbyId(int id) throws SQLException {
+        return executeTransaction(session -> session.get(Admin.class,id));
+
+    }
 }
