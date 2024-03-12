db23f5557134cb3ffdbfb03617e392062bbec733
Sam1ndaFernando <uthpala2001@gmail.com>
2024-03-12 20:58:18 +0530

    bookDao Complete

--- src/main/java/lk/ijse/dao/custom/BookDao.java
+++ src/main/java/lk/ijse/dao/custom/BookDao.java
@@ -3,10 +3,14 @@ package lk.ijse.dao.custom;
 import lk.ijse.dao.CrudDao;
 import lk.ijse.dto.BookDto;
 import lk.ijse.entity.Book;
+import lk.ijse.entity.Branch;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface BookDao extends CrudDao<Book> {
 
+    List<Book> getBookByBranch(Branch branch) throws SQLException;
+
+    List<Book> getByCategory(String category, Branch branch) throws SQLException;
 }
--- src/main/java/lk/ijse/dao/custom/impl/BookDaoImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/BookDaoImpl.java
@@ -1,50 +1,93 @@
 package lk.ijse.dao.custom.impl;
 
+import lk.ijse.dao.BaseDao;
 import lk.ijse.dao.custom.BookDao;
 import lk.ijse.entity.Book;
 import lk.ijse.entity.Branch;
+import org.hibernate.query.Query;
 
 import java.sql.SQLException;
 import java.util.List;
 
+import static lk.ijse.dao.BaseDao.executeTransaction;
+
 public class BookDaoImpl implements BookDao {
     @Override
     public void save(Book entity) throws SQLException {
+        executeTransaction(session -> session.save(entity));
 
     }
 
     @Override
     public void update(Book entity) throws SQLException {
-
+        executeTransaction(session -> {
+            session.update(entity);
+            return null;
+        });
     }
 
     @Override
     public void delete(int id) throws SQLException {
-
+        executeTransaction(session -> {
+            Book entity = null;
+            try {
+                entity = getbyId(id);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            session.delete(entity);
+            return null;
+        });
     }
 
     @Override
     public List<Book> loadAll() throws SQLException {
-        return null;
+        return   executeTransaction(session -> {
+            Query query = session.createQuery("FROM Book ");
+            return query.getResultList();
+
+        });
     }
 
     @Override
     public Book get(String data) throws SQLException {
-        return null;
+        return executeTransaction(session -> {
+            Query query = session.createQuery("from Book where title=:title");
+            query.setParameter("title", data);
+            List<Book> books= query.getResultList();
+            for (Book book : books){
+                return book;
+            }
+            System.out.println("ok");
+            return null;
+        });
     }
 
     @Override
     public Book getbyId(int id) throws SQLException {
-        return null;
+        return executeTransaction(session -> session.get(Book.class,id));
     }
 
     @Override
     public List<Book> getBookByBranch(Branch branch) throws SQLException {
-        return null;
+        return executeTransaction(session -> {
+            Query query = session.createQuery("from Book where branch=:branch");
+            query.setParameter("branch", branch);
+            List<Book> books= query.getResultList();
+            return books;
+
+        });
     }
 
     @Override
     public List<Book> getByCategory(String category, Branch branch) throws SQLException {
-        return null;
+        return executeTransaction(session -> {
+            Query query = session.createQuery("from Book b where b.branch = :branch and b.category = :category");
+            query.setParameter("branch", branch);
+            query.setParameter("category", category);
+            List<Book> books= query.getResultList();
+            return books;
+
+        });
     }
 }
