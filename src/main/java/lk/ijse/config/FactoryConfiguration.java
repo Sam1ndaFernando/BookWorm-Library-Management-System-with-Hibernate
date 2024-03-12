c44d5343928022e77d19b2c8e28ceddcaa62c613
Sam1ndaFernando <uthpala2001@gmail.com>
2024-03-12 20:30:16 +0530

    done

---
+++ src/main/java/lk/ijse/config/FactoryConfiguration.java
@@ -0,0 +1,34 @@
+package lk.ijse.config;
+
+import lk.ijse.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private final SessionFactory sessionFactory;
+
+    private FactoryConfiguration(){
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("hibernate.properties"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());;
+        }
+        Configuration configuration = new Configuration().addProperties(properties).addAnnotatedClass(Admin.class).addAnnotatedClass(Book.class).addAnnotatedClass(Branch.class).addAnnotatedClass(User.class).addAnnotatedClass(Transaction.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null) ? factoryConfiguration = new FactoryConfiguration() : factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
